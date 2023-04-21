c93281efa2270278d80257bb25d5624cdc4085fd
moriyadetteiu <detteiu.stg@gmail.com>
2023-04-22 00:13:22 +0900

    #1 feat: json生成処理を実装

--- scraping/app.py
+++ scraping/app.py
@@ -1,3 +1,7 @@
 from atwiki.dataLibrary import scrape_chips
+import json
 
-scrape_chips(5)
+chips = scrape_chips(5)
+json_chips = json.dumps(chips, ensure_ascii=False)
+file = open('/tmp/data/exe5-chips.json', 'w')
+file.write(json_chips)
